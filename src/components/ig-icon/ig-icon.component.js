a005bbf3da2a51042a965a1bee7c5541b5c1877d
Felipe Pereira <feliperrpereira@gmail.com>
2019-03-17 22:45:29 -0300

    first commit

---
+++ src/components/ig-icon/ig-icon.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { createIconSetFromIcoMoon} from 'react-native-vector-icons'
+import icoMoonConfig from '../../assets/font/selection.json'
+
+const Icon = createIconSetFromIcoMoon(icoMoonConfig, 'MyIcons', 'MyIcons')
+
+export class IgIcon extends Component {
+    render() {
+        const { name, style } = this.props
+        return (
+            <Icon
+                name={name}
+                style={style}
+            />
+        )
+    }
+}
