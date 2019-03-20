b4f3ac76c7d464d7130221d32880289efd333777
Felipe Pereira <pereira.felipe@cwi.com.br>
2019-03-20 12:23:12 -0300

    Ajustando tela de mensagens.

--- src/ui/screen/ig-directs/ig-message.screen.js
+++ src/ui/screen/ig-directs/ig-message.screen.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text, Image, ScrollView, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { View, Text, Image, KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
 
 import api from '../../../api/feed.json';
 import { IgIcon } from '../../components/ig-icon/ig-icon.component.js';
 
 export default class IgMessage extends Component {
+    renderHeader() {
+        const uri = "https://img.freepik.com/free-photo/adorable-jack-russell-retriever-puppy-portrait_53876-64825.jpg?size=338&ext=jpg"
+        return (
+            <View style={styles.headerMessage}>
+                <IgIcon name="left-arrow" style={styles.iconsDirect} />
+                <View style={styles.infoRemetente}>
+                    <Image
+                        style={styles.photoMessage}
+                        source={{ uri: uri }}
+                    />
+                    <Text>joao316</Text>
+                </View>
+                <View style={styles.iconsRight}>
+                    <IgIcon name="photo-camera-empty" style={styles.iconsDirect} />
+                    <IgIcon name="photo-camera-empty" style={styles.iconsDirect} />
+                </View>
+            </View>
+        )
+    }
     renderMessage(message, index) {
         return (
-            <View key={index} style={styles.message}>
-                <Text>{message.mensagem}</Text>
+            <View
+                key={index}
+            >
+                {
+                    <Text style={message.recebido ? styles.leftMassage : styles.rightMassage}>{message.mensagem}</Text> 
+                }
             </View>
         )
     }
     render() {
-        const uri = "https://img.freepik.com/free-photo/adorable-jack-russell-retriever-puppy-portrait_53876-64825.jpg?size=338&ext=jpg"
         return (
             <View style={styles.container}>
-                <View style={styles.headerMessage}>
-                    <IgIcon name="left-arrow" style={styles.iconsDirect} />
-                    <Image 
-                        style={styles.photoMessage}
-                        source={{ uri: uri }} 
-                    />
-                    <Text>joao316</Text>
-                    <IgIcon />
-                    <IgIcon />
-                </View>
-                <ScrollView>
-                    {
-                        api.message.map((message, index) => this.renderMessage(message, index))
-                    }
-                </ScrollView>
+                {this.renderHeader()}
+                <KeyboardAvoidingView style={styles.message} behavior="padding" enabled>
+                    <ScrollView style={styles.scrollMessage}>
+                        {
+                            api.message.map((message, index) => this.renderMessage(message, index))
+                        }
+                    </ScrollView>
+                </KeyboardAvoidingView>
             </View>
         )
     }
@@ -51,8 +66,18 @@ const styles = StyleSheet.create({
         borderColor: "#D8D8D8"
     },
     iconsDirect: {
-        fontSize: 34,
-        color: "black"
+        fontSize: 32,
+        color: "black",
+        marginHorizontal: 2
+    },
+    iconsRight: {
+        flexDirection: "row",
+        alignItems: "flex-end"
+    },
+    infoRemetente: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center"
     },
     photoMessage: {
         width: 42,
@@ -61,7 +86,31 @@ const styles = StyleSheet.create({
         marginHorizontal: 15
     },
     message: {
+        display: "flex",
         flex: 1,
-        flexDirection: "column"
+        flexDirection: "column-reverse",
+        justifyContent: "flex-end",
+        paddingHorizontal: 15,
+    },
+    scrollMessage: {
+        flex: 1,
+        flexDirection: "column-reverse",
+        paddingVertical: 15,
+    },
+    leftMassage: {
+        alignSelf: "flex-start",
+        backgroundColor: "#D8D8D8",
+        paddingVertical: 20,
+        borderRadius: 30,
+        paddingHorizontal: 30,
+        marginTop: 10        
+    },
+    rightMassage: {
+        alignSelf: "flex-end",
+        backgroundColor: "#D8D8D8",
+        paddingVertical: 20,
+        borderRadius: 30,
+        paddingHorizontal: 30,
+        marginTop: 10        
     }
 });
